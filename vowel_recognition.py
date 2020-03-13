c68997aca52cc3b33963e78987370b401bca01e3
Alejandro Martinez <alejandromartinez@vpn-two-factor-general-230-140-127.dartmouth.edu>
2020-03-12 21:05:04 -0400

    added write up

--- vowel_recognition.py
+++ vowel_recognition.py
@@ -1,32 +1,30 @@
 import os
 from vowel_recognition_helpers import *
 import numpy as np
-# data_dim = 16
-# timesteps = 8
-# num_classes = 10
+
+#batch size
 batch_size = 32
 
-df_path = "audio_files/df5.csv"
-prediction_path = "Alexae.wav"
-# os.chdir("..")
+#indicate column names and vowels to classify
+columns = ["audio file","mfcc","vowel label","encoded label"]
+vowels_to_classify = {"ae":0, "ah":1, "aw":2, "eh":3, "er":4, "ei":5, "ih":6, "iy":7, "oa":8, "oo":9, "uh":10, "uw":11}
+
+#paths
+df_path = "audio_files/df.csv"
 cwd = os.getcwd()
 file_path = os.path.join(cwd,'audio_files')
 
-columns = ["audio file","mfcc","vowel label","encoded label"]
+#load df
+print("LOADING DATAFRAME...\n")
 df_path = load_df(file_path,columns,df_path)
-# extract_mfcc(df_path)
-# smooth_features(file_path)
-print("extacting mfccs and splitting train test sets\n")
-vowels_to_classify = {"ae":0, "ah":1, "aw":2, "eh":3, "er":4, "ei":5, "ih":6, "iy":7, "oa":8, "oo":9, "uh":10, "uw":11}
 
+#extract mfccs and split train test sets
+print("EXTRACTING MFCCS AND SPLITTING TRAIN AND TEST SETS...\n")
 X_train, X_test, y_train, y_test = extract_mfcc(df_path,vowels_to_classify)
-top_words = 5000
-print("training")
-print(len(X_train[0]))
-print(len(X_train[0][0]))
-# print(len(X_train[0])
-# print(X_train[0][0])
+
+#create training parameters
 params = {'data dim':len(X_train[0]),'timesteps':len(X_train[0][0]),'num classes':len(vowels_to_classify.keys()),'batch size':32,'input dim':len(X_train)}
-train(df_path, X_train, X_test, y_train, y_test, top_words, params,prediction_path,vowels_to_classify,epochs=10)
-# print(X_test)
-# print(y_test)
+
+#train network
+print("TRAINING NETWORK...")
+train(X_train, X_test, y_train, y_test, params,vowels_to_classify)
--- vowel_recognition_helpers.py
+++ vowel_recognition_helpers.py
@@ -1,129 +1,101 @@
-# import keras
+# imports
 import librosa
 import os
 import pandas as pd
 from time import sleep
 import matplotlib.pyplot as plt
 import librosa.display
-from ast import literal_eval
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from keras.layers.embeddings import Embedding
-from sklearn import preprocessing #import sequence
-from scipy.sparse import csr_matrix
 from keras.preprocessing import sequence
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
+#load dataframe with training samples and labels
 def load_df(file_path,columns,df_path):
     df = pd.DataFrame(columns=columns)
     index = 0
+    #extract wav file path and insert in df
     for subdir,dirs,files in os.walk(file_path):
         for each_dir in dirs:
-            # print(each_dir)
             dir_path = os.path.join(file_path,each_dir)
             for files in os.walk(dir_path):
-                #print(files[2])
                 for each_wav in files[2]:
-                    # print(os.path.join(dir_path,each_wav))
                     label = each_wav[-6:-4]
-                    # print(label)
                     df.loc[index] = [os.path.join(dir_path,each_wav),None,label,None]
                     index += 1
+    #return df path and save to csv
     df_path = os.path.join(df_path)
     df.to_csv(df_path)
     return(df_path)
 
+#extract mfccs from each wav file
 def extract_mfcc(df_path,vowels_to_classify,ratio=0.9,maxlen=12):
     df = pd.read_csv(df_path)
     df = df.astype(object)
+    #load all mfccs into array for later processing
     mfccs = []
+
+    #encode training labels as one hot encoded vectors
     label_encoder = LabelEncoder()
-    # print(list(vowels_to_classify.keys()))
     vowels_encoded = label_encoder.fit_transform(list(vowels_to_classify.keys()))
     onehot_encoder = OneHotEncoder(sparse=False)
     vowels_encoded = vowels_encoded.reshape(len(vowels_encoded), 1)
     onehot_encoded = onehot_encoder.fit_transform(vowels_encoded)
 
-    # print(vowels_encoded)
+    #extract mfccs from each wav file in df
     for index, row in df.iterrows():
         y,sr = librosa.load(df.loc[index,'audio file'],sr=None)
         mfcc = librosa.feature.mfcc(y=y,sr=sr,n_mfcc=40)
 
-        # data = np.array([[1, 2, 3], [5, 6, 7]])
+        #plotting function commented out
+        #plot_mels(mfcc)
 
-        # mfcc = np.average(mfcc, axis=0)
-
-        #print(len(mfcc))
+        #load all mfccs into array for later processing
         mfccs.append(mfcc)
+
+        #save individual mfccs and encodings in df
         df.loc[index,'mfcc'] = mfcc
-        #print(onehot_encoded[vowels_to_classify[df.loc[index,'vowel label']]])
         df.loc[index,'encoded label'] = onehot_encoded[vowels_to_classify[df.loc[index,'vowel label']]]
-        # sleep(10)
 
-
-    #df.apply(np.random.shuffle, axis=0)
-    # length_of_samples = len(mfccs[0])
-    # length_of_vectors = len(mfccs[0][0])
-
+    #separate all training samples and labels
+    #training samples: mfccs
+    #training labels: encodings
     x = mfccs
     y = df['encoded label']
 
-
-    #
-    # encoderX = preprocessing.OneHotEncoder()
-    # X = encoderX.fit_transform(x)
-    # encoderY = preprocessing.LabelEncoder()
-    # y = encoderY.fit_transform(y)
+    #split up train and val sets
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 1-ratio)
+
+    #format train and val tests
+    #applying padded sequences to encodings
+    #convert both samples and encodings to high dimensional numpy arrays
     npx = np.random.random((len(X_train), len(X_train[0]), maxlen))
     npy = np.random.random((len(y_train), 12))
     npxt = np.random.random((len(X_test), len(X_test[0]), maxlen))
     npyt = np.random.random((len(y_test), 12))
-    # print(y_train)
-    #within each row
     for sample in range(len(npx)):
-        #within each sample
         X_train[sample] = sequence.pad_sequences(X_train[sample], maxlen=maxlen)
-
         for mfcc in range(len(npx[sample])):
-
-            #X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
             npx[sample][mfcc] = np.array(X_train[sample][mfcc])
-            # np.array(X_train[sample][mfcc])
-    # print(len(npy))
     for sample in range(len(npy)):
-        #within each sample
-            #convert to one hot encoding
-
             npy[sample] = np.array(y_train)[sample]
     for sample in range(len(npxt)):
-        #within each sample
         X_test[sample] = sequence.pad_sequences(X_test[sample], maxlen=maxlen)
-
         for mfcc in range(len(npxt[sample])):
-
-            #X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
             npxt[sample][mfcc] = np.array(X_test[sample][mfcc])
-            # np.array(X_train[sample][mfcc])
     for sample in range(len(npyt)):
-        #within each sample
-            #convert to one hot encoding
             npyt[sample] = np.array(y_test)[sample]
 
-
-
-
-    # print(type(X_train))
+    #save df and return train and val sets
     df.to_csv(df_path,index=False)
     return(npx, npxt, npy, npyt)
 
 
-        # sleep(10)
-        #plot_mels(mfcc)
 
-
+#plot mfccs
 def plot_mels(mfcc):
         plt.figure(figsize=(10, 4))
         librosa.display.specshow(mfcc, x_axis='time')
@@ -133,41 +105,23 @@ def plot_mels(mfcc):
         plt.show()
 
 
+#train network
+def train(X_train, X_test, y_train, y_test,params,vowels_to_classify,units=100,epochs=15):
 
-def train(df_path, X_train, X_test, y_train, y_test,top_words,params,prediction_path,vowels_to_classify,units=100,epochs=2):
-    df = pd.read_csv(os.path.join(df_path))
-    df = df.astype(object)
-
-    # create the model
-
-
+    #create lstm and dense layer
     model = Sequential()
-    #model.add(Embedding(top_words, length_of_vectors, input_length=length_of_samples))
     model.add(LSTM(units,input_shape=(params['data dim'],params['timesteps'])))
-    # model.add(Dropout(0.2))
-    # model.add(LSTM(params['batch size']))
     model.add(Dense(params['num classes'], activation='sigmoid'))
-    # keras.losses.categorical_crossentropy(y_true, y_pred, from_logits=False, label_smoothing=0)
-    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    print("++++++++")
-    print(type(X_train))
-    print("++++++++")
-    # print(len(X_train))
 
-    # print(len(y_train))
+    #compile model
+    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+    #display model summary
     print(model.summary())
 
+    #fit network
     model.fit(X_train, y_train, epochs=epochs, batch_size=params['batch size'],verbose=1)
+
     # Final evaluation of the model
     scores = model.evaluate(X_test, y_test, verbose=0)
     print("Accuracy: %.2f%%" % (scores[1]*100))
-    #predict
-    y,sr = librosa.load(prediction_path,sr=16000)
-    mfcc = librosa.feature.mfcc(y=y,sr=sr,n_mfcc=40)
-    mfcc = sequence.pad_sequences(mfcc, maxlen=12)
-    mfcc = np.array(mfcc).reshape((1,np.array(mfcc).shape[0], np.array(mfcc).shape[1]))
-    prediction = model.predict(mfcc)
-    # print(vowels_to_classify)
-    print(prediction)
-    idx = np.argmax(prediction)
-    print(f'vowel is: {list(vowels_to_classify.keys())[idx]}')
